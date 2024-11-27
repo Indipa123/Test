9b5cf826750325268009b2bf5ce5c4e55eb2117b
Indipa <indipa2002ayomal@gmail.com>
2024-11-27 18:04:17 +0530

    Initial commit

---
+++ shared/DBconnection.php
@@ -0,0 +1,20 @@
+<?php
+    $server = "localhost";
+    $username = "root";
+    $password = "Ayomal2002";
+    $database = "book_shop";
+
+    try{
+        $connection = mysqli_connect($server, $username, $password, $database);
+
+        if(!$connection){
+            die("Database Connection Error");
+        }
+    }
+    catch(Exception $e){
+        die("Database Connection Error: {$e->getMessage()}");
+    }
+?>
+
+
+
